42cf201ce3d1677f144373c0f4a8d9abb3f4fdfd
kwojcikowski <wojcikowskikacper5@gmail.com>
2023-03-28 22:22:18 +0200

    Working POC

--- src/counter/CounterPage.jsx
+++ src/counter/CounterPage.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import "./CounterPage.css";
 
 export const CounterPage = () => {
 
@@ -8,6 +9,9 @@ export const CounterPage = () => {
         <div className="counter">
             <model-function id="mf1" name="decreaseCount">
                 <button onClick={() => setCount(count - 1)}>-</button>
+                <constraint>
+                    post counterWasDecremented: self.count = self.count@pre - 1
+                </constraint>
             </model-function>
             <span>
                 <model-attribute id="ma1" type="Integer" name="count">
@@ -16,9 +20,15 @@ export const CounterPage = () => {
             </span>
             <model-function id="mf2" name="increaseCount">
                 <button onClick={() => setCount(count + 1)}>+</button>
+                <constraint>
+                    post counterWasIncremented: self.count = self.count@pre + 1
+                </constraint>
             </model-function>
             <model-function id="mf3" name="resetState">
                 <button onClick={() => setCount(0)}>Reset</button>
+                <constraint>
+                    post counterWasReset: self.count = 0
+                </constraint>
             </model-function>
         </div>
     </model>
